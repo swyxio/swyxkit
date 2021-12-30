664460773929e1518e946ba3af6fdd84839e90c6
sw-yx <shawnthe1@gmail.com>
2021-12-31 01:49:09 +0800

    update netlify adapter config for split

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,9 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(),
+		adapter: adapter({
+			split: false
+		}),
 	}
 };
 
