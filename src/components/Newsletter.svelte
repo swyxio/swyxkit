27b85ff0931d7b02648c8585a498e12dde613814
sw-yx <shawnthe1@gmail.com>
2022-01-02 02:48:06 +0800

    fix stuff

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -16,7 +16,7 @@
 						aria-label="Email for newsletter"
 						placeholder="tim@apple.com"
 						autocomplete="email"
-						required=""
+						required={true}
 						class="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
 					/><button
 						class="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
