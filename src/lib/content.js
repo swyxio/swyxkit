a34c1b1d6cd5bf318626312069a6ee6c2f1d1c56
swyx <shawnthe1@gmail.com>
2022-06-11 00:10:21 +0800

    improve youtube embed (#69)
    * improve youtube embed
    
    * Update src/lib/content.js
    
    * Update content.js

--- src/lib/content.js
+++ src/lib/content.js
@@ -98,7 +98,37 @@ export async function getContent(slug) {
 					const videoId = x.startsWith('https://') ? youtube_parser(x) : x;
 					return `<iframe
 			class="w-full object-contain"
-			src="https://www.youtube.com/embed/${videoId}"
+			srcdoc="
+				<style>
+				    body, .youtubeembed {
+					width: 100%;
+					height: 100%;
+					margin: 0;
+					position: absolute;
+					display: flex;
+					justify-content: center;
+					object-fit: cover;
+				    }
+				</style>
+				<a
+				    href='https://www.youtube.com/embed/${videoId}?autoplay=1'
+				    class='youtubeembed'
+				>
+				    <img
+					src='https://img.youtube.com/vi/${videoId}/sddefault.jpg'
+					class='youtubeembed'
+				    />
+				    <svg
+					version='1.1'
+					viewBox='0 0 68 48'
+					width='68px'
+					style='position: relative;'
+				    >
+					<path d='M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z' fill='#f00'></path>
+					<path d='M 45,24 27,14 27,34' fill='#fff'></path>
+				    </svg>
+				</a>
+			"
 			title="video123"
 			name="video123"
 			allow="accelerometer; autoplay; encrypted-media; gyroscope;
