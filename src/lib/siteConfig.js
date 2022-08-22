9d71912ad0ab7e28b6a8f6ab390f2532db1b7a11
Ak4zh <agwl.akash@gmail.com>
2022-08-23 03:02:13 +0530

    Update src/lib/siteConfig.js
    Co-authored-by: swyx <shawnthe1@gmail.com>

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -8,7 +8,7 @@ export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'swyx';
 export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
-export const POST_CATEGORIES = ['Blog']
+export const POST_CATEGORIES = ['Blog'] // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
