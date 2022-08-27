23e147595d4809861ae871f4855ebb87c71463e7
Ak4zh <agwl.akash@gmail.com>
2022-08-27 07:38:05 +0530

    replaced slugify with sindresorhus/slugify

--- src/lib/content.js
+++ src/lib/content.js
@@ -4,7 +4,7 @@ import grayMatter from 'gray-matter';
 import fetch from 'node-fetch';
 import { GH_USER_REPO, APPROVED_POSTERS_GH_USERNAME } from './siteConfig';
 import parse from 'parse-link-header';
-import slugify from 'slugify';
+import slugify from '@sindresorhus/slugify';
 
 import rehypeStringify from 'rehype-stringify';
 import rehypeSlug from 'rehype-slug';
