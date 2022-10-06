dd861e0a808ecf9734db226ef878f80a6aa5ff30
swyx <shawnthe1@gmail.com>
2022-10-06 12:44:18 +0200

    add working histoire version

---
+++ src/components/FeatureCard.story.svelte
@@ -0,0 +1,21 @@
+<script>
+  import FeatureCard from './FeatureCard.svelte';
+  export let Hst
+</script>
+
+<Hst.Story title="FeatureCard">
+  <FeatureCard 
+    title="the Best blogpost of all time"
+    href="http://example.com"
+    stringData="This is a string"
+    ></FeatureCard>
+</Hst.Story>
+
+
+<!-- <script>
+  import FeatureCard from './FeatureCard.svelte'
+  export let Hst
+</script>
+<Hst.Story>
+  <FeatureCard title="Welcome to swyxkit 2022!" href="/welcome" stringData="Jan 2022" />
+</Hst.Story> -->
