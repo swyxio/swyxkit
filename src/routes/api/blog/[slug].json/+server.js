63358899448aa6514a29fa2f27a97d1779c24940
swyx <shawnthe1@gmail.com>
2022-11-28 14:09:37 +0800

    refactor fetch

--- src/routes/api/blog/[slug].json/+server.js
+++ src/routes/api/blog/[slug].json/+server.js
@@ -3,11 +3,11 @@ import { error } from '@sveltejs/kit';
 /**
  * @type {import('@sveltejs/kit').RequestHandler}
  */
-export async function GET({ params }) {
+export async function GET({ fetch, params }) {
 	const { slug } = params;
 	let data;
 	try {
-		data = await getContent(slug);
+		data = await getContent(fetch, slug);
 		return new Response(JSON.stringify(data), {
 			headers: {
 				'Cache-Control': `max-age=0, s-maxage=${60}` // 1 minute.. for now
