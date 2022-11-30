f774b4f399b465e21a111daf94de58c24f275501
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-11-30 10:36:57 +0100

    Add sveltekit-search-params to blog page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
+import { ssp } from "sveltekit-search-params/plugin";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [ssp(), sveltekit()]
 };
 
 export default config;
