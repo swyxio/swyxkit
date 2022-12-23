6b49d415f2e46edfd397c28f9997a0c1e5bc8cc1
swyx <shawnthe1@gmail.com>
2022-12-23 13:33:07 -0500

    try to fix deps

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import fs from 'fs';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [ssp(), sveltekit(), rawFonts(['.ttf']), nativeNodeModulesPlugin],
+	plugins: [ssp(), sveltekit(), rawFonts(['.ttf']),
+		// nativeNodeModulesPlugin
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
@@ -34,40 +36,40 @@ function rawFonts(ext) {
 	};
 }
 
-// from https://github.com/yisibl/resvg-js/issues/175
-// solution is https://github.com/evanw/esbuild/issues/1051
-const nativeNodeModulesPlugin = {
-	name: 'native-node-modules',
-	setup(build) {
-		// If a ".node" file is imported within a module in the "file" namespace, resolve 
-		// it to an absolute path and put it into the "node-file" virtual namespace.
-		build.onResolve({ filter: /\.node$/, namespace: 'file' }, args => ({
-			path: require.resolve(args.path, { paths: [args.resolveDir] }),
-			namespace: 'node-file',
-		}))
+// // from https://github.com/yisibl/resvg-js/issues/175
+// // solution is https://github.com/evanw/esbuild/issues/1051
+// const nativeNodeModulesPlugin = {
+// 	name: 'native-node-modules',
+// 	setup(build) {
+// 		// If a ".node" file is imported within a module in the "file" namespace, resolve 
+// 		// it to an absolute path and put it into the "node-file" virtual namespace.
+// 		build.onResolve({ filter: /\.node$/, namespace: 'file' }, args => ({
+// 			path: require.resolve(args.path, { paths: [args.resolveDir] }),
+// 			namespace: 'node-file',
+// 		}))
 
-		// Files in the "node-file" virtual namespace call "require()" on the
-		// path from esbuild of the ".node" file in the output directory.
-		build.onLoad({ filter: /.*/, namespace: 'node-file' }, args => ({
-			contents: `
-        import path from ${JSON.stringify(args.path)}
-        try { module.exports = require(path) }
-        catch {}
-      `,
-		}))
+// 		// Files in the "node-file" virtual namespace call "require()" on the
+// 		// path from esbuild of the ".node" file in the output directory.
+// 		build.onLoad({ filter: /.*/, namespace: 'node-file' }, args => ({
+// 			contents: `
+//         import path from ${JSON.stringify(args.path)}
+//         try { module.exports = require(path) }
+//         catch {}
+//       `,
+// 		}))
 
-		// If a ".node" file is imported within a module in the "node-file" namespace, put
-		// it in the "file" namespace where esbuild's default loading behavior will handle
-		// it. It is already an absolute path since we resolved it to one above.
-		build.onResolve({ filter: /\.node$/, namespace: 'node-file' }, args => ({
-			path: args.path,
-			namespace: 'file',
-		}))
+// 		// If a ".node" file is imported within a module in the "node-file" namespace, put
+// 		// it in the "file" namespace where esbuild's default loading behavior will handle
+// 		// it. It is already an absolute path since we resolved it to one above.
+// 		build.onResolve({ filter: /\.node$/, namespace: 'node-file' }, args => ({
+// 			path: args.path,
+// 			namespace: 'file',
+// 		}))
 
-		// Tell esbuild's default loading behavior to use the "file" loader for
-		// these ".node" files.
-		let opts = build.initialOptions
-		opts.loader = opts.loader || {}
-		opts.loader['.node'] = 'file'
-	},
-}
+// 		// Tell esbuild's default loading behavior to use the "file" loader for
+// 		// these ".node" files.
+// 		let opts = build.initialOptions
+// 		opts.loader = opts.loader || {}
+// 		opts.loader['.node'] = 'file'
+// 	},
+// }
